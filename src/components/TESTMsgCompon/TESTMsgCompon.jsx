a8fcfdf968cd1a554a1cfecbb685fc5916a53a72
AllenJoeG <allen.joeg@gmail.com>
2022-01-30 18:02:07 -0600

    test pieces

--- src/components/TESTMsgCompon/TESTMsgCompon.jsx
+++ src/components/TESTMsgCompon/TESTMsgCompon.jsx
@@ -1,11 +1,12 @@
 import react from 'react';
+import { Box, Typography } from '@mui/material';
 
 export default function TestMsgCompon({timestamp, message}) {
 
   return(
-    <>
-    <p>{message}</p>
-    <p>{timestamp}</p>
-    </>
+    <Box>
+      <p>{message}</p>
+      <p>{timestamp}</p>
+    </Box>
   )
 }
