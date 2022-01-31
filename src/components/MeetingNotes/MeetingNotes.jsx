c3e97663db81c845ddc5802c915c0c2deb692ac4
Michael Huso <husomichael@gmail.com>
2022-01-31 11:58:03 -0600

    Clear notes added

--- src/components/MeetingNotes/MeetingNotes.jsx
+++ src/components/MeetingNotes/MeetingNotes.jsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react';
+import { useEffect} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useHistory, useParams} from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import { Box, Button, TextField, Typography } from '@mui/material';
 
 
@@ -40,17 +40,23 @@ function MeetingNotes() {
     })
   };
 
+  function handleClearNotes(){
+    dispatch({
+      type: 'CLEAR_EDIT_NOTES',
+    })
+    //TODO: Save notes after??
+  };
+
   return (
     <div>
       <Box
         display="flex"
         justifyContent="center"
         alignItems="center"
-      // minHeight="15vh"
       >
         <Typography
-          variant="subtitle1"
-          component="div"
+          variant="h6"
+          component="h6"
         >
           Notes
         </Typography>
@@ -59,27 +65,35 @@ function MeetingNotes() {
         display="flex"
         justifyContent="center"
         alignItems="center"
-      // minHeight="0vh"
       >
-          <TextField
-            placeholder='Notes'
-            value={notes.notes || ''}
-            onChange={handleNoteChange}
-            sx={{ mr: 3, width: 250 }}
-          />
-          <Button
-            variant="contained"
-            onClick={handleSaveNotes}
-          >
-            Save
-          </Button>
+        <TextField
+          multiline={true} //Allows changing height of TextField.
+          rows={15} //Change height of TextField here.
+          placeholder='Notes'
+          value={notes.notes || ''}
+          onChange={handleNoteChange}
+          sx={{mt: 1, width: 250,}} //Change width of TextField here.
+        />
       </Box>
       <Box
         display="flex"
         justifyContent="center"
         alignItems="center"
-      // minHeight="20vh"
       >
+        <Button
+          variant=""
+          onClick={handleClearNotes}
+          sx={{mt: 2, mr: 2}}
+        >
+          Clear
+        </Button>
+        <Button //TODO: Add sweetalerts or something to notify changes saved.
+          variant="contained"
+          onClick={handleSaveNotes}
+          sx={{mt: 2, ml: 10}}
+        >
+          Save
+        </Button>
       </Box>
     </div>
   )
