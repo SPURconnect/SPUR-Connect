9a5bc8854ff3d6b169bf738a70adbed64a672b7d
Nicholas Ilacqua <Niservices88@gmail.com>
2022-02-01 15:14:50 -0600

    working on the industries drop down on the register page

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -5,9 +5,12 @@ function RegisterForm() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [email, setEmail] = useState('');
-  const [location, setLocation] = useState('');
+  const [city, setCity] = useState('');
+  const [state, setState] = useState('');
+  const [zip, setZip] = useState('');
   const [industry, setIndustry] = useState(1);
   const errors = useSelector((store) => store.errors);
+  const industriesReducer = useSelector(store => store.industriesReducer);
   const dispatch = useDispatch();
 
   const registerUser = (event) => {
@@ -19,11 +22,22 @@ function RegisterForm() {
         username: username,
         password: password,
         email: email,
-        location_city: location,
-        industry_id: industry,
+        location_city: city,
+        location_state: state,
+        location_zip: zip,
+        industry_name:industry
+  
       },
     });
   }; // end registerUser
+
+  const handleIndustryChange = (event) => {
+    event.preventDefault();
+    dispatch({
+      type: 'SORT_BY_INDUSTRY',
+      payload: event.target.value
+    });
+  }
 
   return (
     <form className="formPanel" onSubmit={registerUser}>
@@ -71,29 +85,53 @@ function RegisterForm() {
         </label>
       </div>
       <div>
-        <label htmlFor="location">
+          <label htmlFor="Location/City">
           Location/City:<br></br>
           <input
-            type="location"
-            name="location"
-            value={location}
+              type="Location/City"
+              name="Location/City"
+              value={city}
             required
-            onChange={(event) => setLocation(event.target.value)}
+              onChange={(event) => setCity(event.target.value)}
           />
         </label>
       </div>
-
+        <div>
+          <label htmlFor="Location/State">
+            Location/State:<br></br>
+            <input
+              type="Location/State"
+              name="Location/State"
+              value={state}
+              required
+              onChange={(event) => setState(event.target.value)}
+            />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="Location/Zip">
+            Location/Zip:<br></br>
+            <input
+              type="Location/Zip"
+              name="Location/Zip"
+              value={zip}
+              required
+              onChange={(event) => setZip(event.target.value)}
+            />
+          </label>
+        </div>
       <div><br></br>
         <label htmlFor="Industry">
         Industry:<br></br>
+          <select onChange={(event) => handleIndustryChange(event)}>
+            <option disabled>Filter By Industry</option>
+            {industriesReducer.map((industry) => {
+              return (
+                <option key={industry.id} value={industry.id}>{industry.industry_name}</option>
+              )
+            })}
+            </select>
         </label>
-          <input
-            type="industry"
-            name="industry"
-            value={industry}
-            required
-            onChange={(event) => setIndustry(event.target.value)}
-          />
         
       </div>
       <div>
