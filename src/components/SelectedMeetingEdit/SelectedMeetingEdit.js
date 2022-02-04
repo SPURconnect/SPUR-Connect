a1a97c672ef257b101a490238a0eb021f73315e9
Victor Lees <vlees14.pa@gmail.com>
2022-02-04 14:42:25 -0500

    updated selectedmeetingedit no state

--- src/components/SelectedMeetingEdit/SelectedMeetingEdit.js
+++ src/components/SelectedMeetingEdit/SelectedMeetingEdit.js
@@ -18,25 +18,59 @@ function SelectedMeetingEdit() {
   const dispatch = useDispatch();
   const params = useParams();
   const meetings = useSelector(store => store.meetings);
+  const meetingDetailsReducer = useSelector(store => store.meetingDetailsReducer);
 
-
+/* 
   const [location, setLocation] = useState('');
   const [date, setDate] = useState('');
-  const [summary, setSummary] = useState('');
+  const [summary, setSummary] = useState(''); */
+
+  useEffect(() => {
+    dispatch({
+      type: 'FETCH_MEETING_DETAILS',
+      payload: params.id
+    })
+  }, [params.id]);
+
+  
+
+  const handleMeetupLocation = (e) => {
+    dispatch({
+      type: 'SET_MEETINGUP_LOCATION',
+      payload: e.target.value
+    })
+  }
+
+  const handleDate = (e) => {
+    dispatch({
+      type: 'SET_DATE',
+      payload: e.target.value
+    })
+  }
+
+  const handleSummary = (e) => {
+    dispatch({
+      type: 'SET_SUMMARY',
+      payload: e.target.value
+    })
+  }
+
+
     
 console.log('meeting reducer')
-  const editDeatils = (e) => {
+  const saveMeetingDeatils = (e) => {
    e.preventDefault();
       dispatch({
-        type: 'EDIT_MEETING_DETAILS',
+        type: 'SAVE_MEETING_DETAILS',
         payload: {
-          meetup_location: location,
-          date: date,
-          summary: summary,
-          id: params.id,
+          meetup_location: meetingDetailsReducer.meetup_location,
+          date: meetingDetailsReducer.date,
+          summary:meetingDetailsReducer.summary,
+          id: params.id
         }
     
       })
+      history.push(`/meeting/${params.id}`);
     }
 
 
@@ -74,8 +108,8 @@ console.log('meeting reducer')
           multiline={true} //Allows changing height of TextField.
           rows={1} //Change height of TextField here.
           placeholder='Location'
-          value={location}
-          onChange={(e) => setLocation(e.target.value)}
+          value={meetingDetailsReducer.meetup_location}
+          onChange={handleMeetupLocation}
           sx={{mt: 1, width: 250,}} //Change width of TextField here.
         />
       </Box>
@@ -88,10 +122,8 @@ console.log('meeting reducer')
      <LocalizationProvider dateAdapter={AdapterDateFns}>
       <DatePicker
         label="Meeting Date"
-        value={date}
-        onChange={(newDate) => {
-          setDate(newDate);
-        }}
+        value={meetingDetailsReducer.date}
+        onChange={handleDate}
         renderInput={(params) => <TextField {...params} />}
       />
     </LocalizationProvider>
@@ -107,8 +139,8 @@ console.log('meeting reducer')
           multiline={true} //Allows changing height of TextField.
           rows={5} //Change height of TextField here.
           placeholder='summary'
-          value={summary}
-          onChange={(e) => setSummary(e.target.value)}
+          value={meetingDetailsReducer.summary}
+          onChange={handleSummary}
           sx={{mt: 1, width: 250,}} //Change width of TextField here.
         /><br></br>
 
@@ -124,7 +156,7 @@ console.log('meeting reducer')
               <Button
                variant="contained"
                style={{ backgroundColor: '#0583f2', color: 'White' }}               
-               onClick={(e) => { editDeatils(e) }}>Add</Button>        
+               onClick={saveMeetingDeatils}>Add</Button>        
               
       </Box>
      </div>
