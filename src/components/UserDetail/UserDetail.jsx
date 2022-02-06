a8ffca99e4e2b6ed1113c757b7306c127987c029
AllenJoeG <allen.joeg@gmail.com>
2022-02-06 12:06:50 -0600

    Structural Grid styling pass on UserDetail/UserPage

--- src/components/UserDetail/UserDetail.jsx
+++ src/components/UserDetail/UserDetail.jsx
@@ -2,13 +2,20 @@ import React, { useEffect } from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom'; 
-import { styled } from '@mui/material/styles';
-import FormGroup from '@mui/material/FormGroup';
-import { Box, Button, TextField, Typography, ListItemAvatar, Avatar } from '@mui/material';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Switch from '@mui/material/Switch';
-import Stack from '@mui/material/Stack';
 
+//MUI Stuff
+import { styled } from '@mui/material/styles';
+import { Grid, FormControlLabel, Switch, Stack, FormGroup, Box, Button, TextField, Typography, 
+  ListItemAvatar, Avatar } from '@mui/material';
+
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
+////
 
 function UserDetail ({profile}){
   const history = useHistory();
@@ -86,37 +93,147 @@ function UserDetail ({profile}){
   }
 
   return(
-    <div>
-      <ListItemAvatar>
-        <Avatar 
-        sx={{ width: 200, height: 200 }}
-        src={profile.photo} />
-      </ListItemAvatar>
-      <h3>{profile.first_name}  {profile.last_name}</h3>
-      <ul>
-        <li>{profile.location_city}, {profile.location_state}</li>
-        <li>{profile.industry_name} </li>
-        <li>{profile.email}</li>
-        <li>{profile.linkedin}, {profile.twitter}, {profile.instagram}, {profile.youtube}, {profile.facebook}</li>
-        <li>{profile.portfolio}</li>
-        <li>{profile.about_me}</li>
-      </ul>
-      <FormGroup onChange={(event) => setAvailability(event)}>
-        <Stack direction="row" spacing={1} alignItems="center">
-          <h3>Availability</h3>
-          {setSwitch()}
-        </Stack>
-      </FormGroup>
+    <Grid container>
+      {/* Row 1 */}
+      <Grid item xs={3}/>
+      <Grid item xs={6} mt="10px" align="center">
+        <ListItemAvatar>
+          <Avatar
+          sx={{ width: 175, height: 175 }}
+          src={profile.photo} />
+        </ListItemAvatar>
+      </Grid>
+      <Grid item xs={3}/>
+      {/*  */}
 
-    <div>
+      {/* Row 2 */}
+      <Grid item xs={3}/>
+      <Grid item xs={6} align="center">
+        <h3>
+          {profile.first_name} {profile.last_name}
+        </h3>
+      </Grid>
+      <Grid item xs={3}/>
+      {/*  */}
+
+      {/* Row 2 */}
+      <Grid item xs={.5}/>
+      <Grid item xs={4.5}>
+        {profile.industry_name}
+      </Grid>
+      <Grid item xs={.5}/>
+      <Grid item xs={6} align="right">
+        {profile.location_city}, 
+          {profile.location_state}
+      </Grid>
+      <Grid item xs={.5}/>
+      {/*  */}
+
+      {/* Row 3 */}
+      <Grid item xs={.5}/>
+      <Grid item xs={11} mt="15px">
+        <TextField
+        multiline
+        label={`About ${profile.first_name}`}
+        fullWidth
+        value={profile.about_me}/>
+      </Grid>
+      <Grid item xs={.5}/>
+      
+      {/*  */}
+    
+      {/* Row 4 */}
+      <Grid item xs={.5}/>
+      <Grid item xs={5.25} mt="15px">
+        <AlternateEmailIcon/>
+        {profile.email}
+      </Grid>
+      <Grid item xs={.5}/>
+      <Grid item xs={5.25} mt="15px">
+        <YouTubeIcon/>
+        {profile.youtube}
+      </Grid>
+      <Grid item xs={.5}/>
+      
+      {/*  */}
+
+      {/* Row 5 */}
+      <Grid item xs={.5}/>
+      <Grid item xs={5.25} mt="15px">
+        <FacebookIcon/>
+        {profile.facebook}
+      </Grid>
+      <Grid item xs={.5}/>
+      <Grid item xs={5.25} mt="15px">
+        <InstagramIcon/>
+        {profile.instagram}
+      </Grid>
+      <Grid item xs={.5}/>
+      
+      {/*  */}
+
+      {/* Row 6 */}
+      <Grid item xs={.5}/>
+      <Grid item xs={5.25} mt="15px">
+        <LinkedInIcon/>
+        {profile.linkedin}
+      </Grid>
+      <Grid item xs={.5}/>
+      <Grid item xs={5.25} mt="15px">
+        <TwitterIcon/>
+        {profile.twitter}
+      </Grid>
+      <Grid item xs={.5}/>        
+      {/*  */}
+
+      {/* Row 7 */}
+      <Grid item xs={.5}/>
+      <Grid item xs={5.25} mt="15px">
+        <GitHubIcon/>
+        {profile.portfolio}
+      </Grid>
+      <Grid item xs={.5}/>
+      <Grid item xs={5.25} mt="15px">
+        
+      </Grid>
+      <Grid item xs={.5}/>        
+      
+      {/*  */}
+
+      {/* Row 8 */}
+      <Grid item xs={.5}/>
+      <Grid item xs={11}>
+        <FormGroup onChange={(event) => setAvailability(event)}>
+          <Stack direction="row" spacing={1} alignItems="center">
+            <h4>Show in searches. <br/>
+              Allow meeting/messages.</h4>
+            {setSwitch()}
+          </Stack>
+        </FormGroup>
+      </Grid>
+      <Grid item xs={.5}/>
+      {/*  */}
+  
+      {/* Row 9 */}
+      <Grid item xs={.5}/>
+      <Grid item xs={5.25}>
         <Button
+          size="small"
           variant="contained"
           onClick={()=> history.push(`/edit/${profile.id}`)}
         >
-          Edit Profile Information
+          Edit Profile Info
         </Button>
-    </div>
-    </div>
+      </Grid>
+      <Grid item xs={.5}/>
+      <Grid item xs={5.25}>
+        <LogOutButton className="btn" />
+      </Grid>
+      
+      <Grid item xs={.5}/>
+      {/*  */}
+
+    </Grid>
   )
 }
 
