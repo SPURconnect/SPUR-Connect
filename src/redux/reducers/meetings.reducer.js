90e16145d3539a7632c57a4735be1c888b3ba6c6
Nicholas Ilacqua <Niservices88@gmail.com>
2022-02-07 15:42:21 -0600

    updated meetings reducer

--- src/redux/reducers/meetings.reducer.js
+++ src/redux/reducers/meetings.reducer.js
@@ -2,8 +2,6 @@ const meetingsReducer = (state = [], action) => {
   switch (action.type) {
     case 'SET_MEETINGS':
       return action.payload;
-    case 'SET_SINGLE_MEETING':
-      return action.payload;
     case 'CLEAR_MEETINGS':
       return [];
     default:
