cafed62caf6bf2bebc687dcf09d9b5b6de4529dd
Michael Huso <husomichael@gmail.com>
2022-02-09 14:53:08 -0600

    menu and menuitem added and tied to card media

--- src/components/MeetingPhotosItem/MeetingPhotosItem.jsx
+++ src/components/MeetingPhotosItem/MeetingPhotosItem.jsx
@@ -1,7 +1,7 @@
-import { useEffect} from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory, useParams } from 'react-router-dom';
-import { Box, Button, Card, CardActionArea, CardMedia, CardContent, CardActions, Typography} from '@mui/material';
+import { Box, Button, Card, CardActionArea, CardMedia, CardContent, CardActions, Typography, List, ListItem, ListItemText, MenuItem, Menu} from '@mui/material';
 
 
 function MeetingPhotosItem({photo}) {
@@ -9,6 +9,8 @@ function MeetingPhotosItem({photo}) {
   const history = useHistory();
   const dispatch = useDispatch();
   const params = useParams();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
 
   const openInNewTab = () => {
     const newWindow = window.open(photo.image_url, '_blank', 'noopener,noreferrer')
@@ -22,6 +24,14 @@ function MeetingPhotosItem({photo}) {
     })
   }
 
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <Box
       display="flex"
@@ -32,19 +42,49 @@ function MeetingPhotosItem({photo}) {
       <Card sx={{ maxWidth: '100%', boxshadow: 3 }}>
         <CardActionArea>
           <CardMedia
+            aria-controls={open ? 'demo-positioned-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
             component="img"
             height="140"
             image={photo.image_url}
             alt="Open Full Size Image"
-            onClick={openInNewTab}
+            onClick={handleClick}
           />
         </CardActionArea>
         <CardActions>
-          <Button size="small" color="error" variant="contained" onClick={handleDeleteButton} sx={{ml: 3.2}}> 
+          <Button 
+            size="small" 
+            color="error" 
+            variant="contained" 
+            onClick={handleDeleteButton} 
+            sx={{
+              ml: 3.2
+            }}
+          > 
             Delete
           </Button>
         </CardActions>
       </Card>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+      >
+        <MenuItem onClick={handleClose}>View Full Size</MenuItem>
+        <MenuItem onClick={handleDeleteButton}>Delete</MenuItem>
+        <MenuItem onClick={handleClose}>Cancel</MenuItem>
+      </Menu>
     </Box>
   )
 };
