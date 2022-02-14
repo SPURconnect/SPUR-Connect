29cbaf40a59ffe8cdbac267ab4872c8083ce07ec
Kayla Mir <kayla.mir32@gmail.com>
2022-02-14 14:24:47 -0600

    added green border/color to messages recieved

--- src/components/MessagesMsg/MessagesMsg.jsx
+++ src/components/MessagesMsg/MessagesMsg.jsx
@@ -1,41 +1,58 @@
 import react from 'react';
 import { TableCell, TableRow, Box, Typography, List, ListItem, ListItemText } from '@mui/material';
 
-export default function MessagesMsg({timestamp, message, alignment}) {
+export default function MessagesMsg({ timestamp, message, alignment, user }) {
 
-  return(
+  return (
     <>
-      <TableRow
-        width="95%"
-      >
-        <TableCell
-          sx={{mx: 0, my: 0, py: 0, px: 0, width: "95%"}}
-          align={alignment}
+      {user === 'receiver' ?
+        <TableRow
+          width="95%"
         >
-        <List><ListItem>
-          <ListItemText
-            sx={{mx: 0, my: 0, paddingx: 0, paddingy: 0}}
+          <TableCell
+            sx={{ mx: 0, my: 0, py: 0, px: 0, width: "95%" }}
             align={alignment}
-            primary={message}
-            secondary={timestamp.slice(0, 10)}
-          />
-        </ListItem></List>
-        </TableCell>
-      </TableRow>
-    
+          >
+            <List><ListItem>
+              <ListItemText
+                sx={{ mx: 0, my: 0, paddingx: 0, paddingy: 0 }}
+                align={alignment}
+                primary={message}
+                secondary={timestamp.slice(0, 10)}
+              />
+            </ListItem></List>
+          </TableCell>
+        </TableRow>
+        :
+        <TableRow
+          width="95%"
+        >
+          <TableCell
+            sx={{ mx: 0, my: 0, py: 0, px: 0, width: "95%" }}
+            align={alignment}
+          >
+            <List><ListItem sx={{ width: '80%' }}>
+              <ListItemText
+                sx={{
+                  mx: 0,
+                  my: 0,
+                  paddingx: 0,
+                  paddingy: 0,
+                  border: '2px solid lightGreen',
+                  backgroundColor: 'lightGreen',
+                  borderRadius: '15px',
+                  padding: "10px",
+                  width: '75%'
+                }}
+                align={alignment}
+                primary={message}
+                secondary={timestamp.slice(0, 10)}
+              />
+            </ListItem></List>
+          </TableCell>
+        </TableRow>
+      }
     </>
   )
 }
 
-//        <TableRow
-//         width="100%"
-//       >
-//         <TableCell
-//           width="95%"
-//           sx={{}}
-//           align={alignment}
-//         >
-//           {timestamp}
-//         </TableCell>
-//       </TableRow>
-          
